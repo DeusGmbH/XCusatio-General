ae9fec5977cab1ec1d15b71e462e41e95a2530a3
tobiasxschmidt <tsbaunatal@gmail.com>
2018-03-17 12:11:52 +0100

    Added ScenarioType Enum

--- src/main/java/com/deusgmbh/xcusatio/data/scenarios/Scenario.java
+++ src/main/java/com/deusgmbh/xcusatio/data/scenarios/Scenario.java
@@ -2,4 +2,7 @@ package com.deusgmbh.xcusatio.data.scenarios;
 
 public class Scenario {
 
+    public enum ScenarioType {
+        WheelOfFurtune, ThumbGesture, LateArrival, DelayedSubmission
+    }
 }
