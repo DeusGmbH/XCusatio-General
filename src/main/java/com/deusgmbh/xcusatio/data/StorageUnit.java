64b97f5062c1a5dab5436f7dbebc185cd01ee781
tobiasxschmidt <tsbaunatal@gmail.com>
2018-03-17 12:14:47 +0100

    Added comments and some base functions that have to be implemented

--- src/main/java/com/deusgmbh/xcusatio/data/StorageUnit.java
+++ src/main/java/com/deusgmbh/xcusatio/data/StorageUnit.java
@@ -22,17 +22,63 @@ public class StorageUnit<T> {
         this("");
     }
 
+    /**
+     * Adds a unit to the units array
+     * 
+     * @param unit
+     * @return <b>this</b>
+     */
+    public StorageUnit<T> add(T unit) {
+        units.add(unit);
+        return this;
+    }
+
+    /**
+     * 
+     * @param unit
+     * @return <b>this</b>
+     */
+    public StorageUnit<T> remove(T unit) {
+        // TODO: implement
+        return this;
+    }
+
+    /**
+     * 
+     * @param id
+     * @return element with id
+     */
+    public T get(int id) {
+        return units.get(id);
+    }
+
+    /**
+     * Loads XML file from path if present and parses it into the units array
+     * 
+     * @return <b>this</b>
+     */
     public StorageUnit<T> load() {
 
         return this;
     }
 
+    /**
+     * This should persist the units array to path
+     * 
+     * @return <b>this</b>
+     */
     public StorageUnit<T> persist() {
         Class<T> typeClass = this.getGenericTypeClass();
         xstream.alias(typeClass.getSimpleName(), typeClass);
+        // not finished
+
         return this;
     }
 
+    /**
+     * 
+     * @returns the class of the generic type of <b>this</b>
+     */
     @SuppressWarnings("unchecked")
     private Class<T> getGenericTypeClass() {
         try {
