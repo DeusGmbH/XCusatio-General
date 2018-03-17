88e2ebc33448030db4cc8dae44465d6768ed9d0f
tobiasxschmidt <tsbaunatal@gmail.com>
2018-03-17 15:00:31 +0100

    Added Data model for Excuses

--- src/main/java/com/deusgmbh/xcusatio/data/excuses/Excuse.java
+++ src/main/java/com/deusgmbh/xcusatio/data/excuses/Excuse.java
@@ -1,5 +1,35 @@
 package com.deusgmbh.xcusatio.data.excuses;
 
+import java.util.HashSet;
+import java.util.Set;;
+
 public class Excuse {
+    private String text;
+    private Set<String> tags;
+
+    public Excuse(String text) {
+        this(text, new HashSet<String>());
+    }
+
+    public Excuse(String text, Set<String> tags) {
+        this.text = text;
+        this.tags = tags;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
 
 }
