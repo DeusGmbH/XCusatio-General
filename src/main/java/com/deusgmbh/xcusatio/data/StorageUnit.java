b94d956109a8dc9d97bb040500861135eb2eb709
larsdittert <larsdittert@users.noreply.github.com>
2018-03-22 18:38:49 +0100

    Implement FileIn- and FileOutputstream

--- src/main/java/com/deusgmbh/xcusatio/data/StorageUnit.java
+++ src/main/java/com/deusgmbh/xcusatio/data/StorageUnit.java
@@ -1,5 +1,11 @@
 package com.deusgmbh.xcusatio.data;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,67 +15,103 @@ import com.thoughtworks.xstream.XStream;
 
 /**
  * 
- * @author Tobias.Schmidt@de.ibm.com
+ * @author Lars.Dittert@de.ibm.com
  *
  */
 public class StorageUnit<T> {
-    private static final Logger LOGGER = Logger.getLogger(StorageUnit.class.getName());
-    protected final String path;
+	private static final Logger LOGGER = Logger.getLogger(StorageUnit.class.getName());
+	protected final String path;
 
-    protected ArrayList<T> units = new ArrayList<T>();
-    protected XStream xstream;
+	protected ArrayList<T> units = new ArrayList<T>();
+	protected XStream xstream;
 
-    public StorageUnit(String path) {
-        this.path = path;
-        this.load();
-    }
+	private Class<T> parameterType;
 
-    public StorageUnit() {
-        this("");
-    }
+	public StorageUnit(String path) throws FileNotFoundException, IOException {
+		this.path = path;
+		this.load();
+	}
 
-    public List<T> getUnits() {
-        return this.units;
-    }
+	public StorageUnit() throws FileNotFoundException, IOException {
+		this("");
+	}
 
-    /**
-     * Loads XML file from path if present and parses it into the units array
-     * 
-     * @return <b>this</b>
-     */
-    public StorageUnit<T> load() {
+	public List<T> getUnits() {
+		return this.units;
+	}
 
-        return this;
-    }
+	/**
+	 * Loads XML file from path if present and parses it into the units array
+	 * 
+	 * @return <b>this</b>
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	@SuppressWarnings("unchecked")
+	public StorageUnit<T> load() throws FileNotFoundException, IOException {
 
-    /**
-     * This should persist the units array to path
-     * 
-     * @return <b>this</b>
-     */
-    public StorageUnit<T> persist() {
-        Class<T> typeClass = this.getGenericTypeClass();
-        xstream.alias(typeClass.getSimpleName(), typeClass);
-        // not finished
+		BufferedReader reader = new BufferedReader(new FileReader(parameterType.getSimpleName() + ".xml"));
+		String line = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		String ls = System.getProperty("line.separator");
 
-        return this;
-    }
+		try {
+			while ((line = reader.readLine()) != null) {
+				stringBuilder.append(line);
+				stringBuilder.append(ls);
+			}
 
-    /**
-     * 
-     * @returns the class of the generic type of <b>this</b>
-     */
-    @SuppressWarnings("unchecked")
-    private Class<T> getGenericTypeClass() {
-        try {
-            String className = ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]
-                    .getTypeName();
-            LOGGER.info("ParamterType of Storage Unit is " + className);
-            Class<?> clazz = Class.forName(className);
-            return (Class<T>) clazz;
-        } catch (Exception e) {
-            throw new IllegalStateException("Class is not parametrized with generic type!!! Please use extends <> ");
-        }
-    }
+			units = (ArrayList<T>) xstream.fromXML(stringBuilder.toString());
+		} finally {
+			reader.close();
+		}
+		LOGGER.info("XML is load from " + parameterType.getSimpleName() + ".xml");
+		return this;
+	}
+
+	/**
+	 * This should persist the units array to path
+	 * 
+	 * @return <b>this</b>
+	 */
+	public StorageUnit<T> persist() {
+		xstream.alias(parameterType.getSimpleName(), parameterType);
+		String dataXml = xstream.toXML(this.units);
+		File file = new File(parameterType.getSimpleName() + ".xml");
+
+		try (FileOutputStream fop = new FileOutputStream(file)) {
+
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			byte[] contentInBytes = dataXml.getBytes();
+
+			fop.write(contentInBytes);
+			fop.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		LOGGER.info("XML is saved into " + parameterType.getSimpleName() + ".xml");
+		return this;
+	}
+
+	/**
+	 * 
+	 * @returns the class of the generic type of <b>this</b>
+	 */
+	@SuppressWarnings("unchecked")
+	private Class<T> getGenericTypeClass() {
+		try {
+			String className = ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]
+					.getTypeName();
+			LOGGER.info("ParamterType of Storage Unit is " + className);
+			Class<?> clazz = Class.forName(className);
+			return (Class<T>) clazz;
+		} catch (Exception e) {
+			throw new IllegalStateException("Class is not parametrized with generic type!!! Please use extends <> ");
+		}
+	}
 
 }
