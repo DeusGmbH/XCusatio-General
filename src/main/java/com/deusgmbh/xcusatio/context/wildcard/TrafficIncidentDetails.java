4fdd04d3c34ced5acc97ca32b7a9867b51961e11
affeb <affeb@DESKTOP-AJ3K01H>
2018-03-25 20:40:04 +0200

    dummies for each context class have been added

--- src/main/java/com/deusgmbh/xcusatio/context/wildcard/TrafficIncidentDetails.java
+++ src/main/java/com/deusgmbh/xcusatio/context/wildcard/TrafficIncidentDetails.java
@@ -3,10 +3,7 @@ package com.deusgmbh.xcusatio.context.wildcard;
 /**
  * 
  * @author jan.leiblein@gmail.com
- * 
- * */
-
-import com.deusgmbh.xcusatio.context.wildcard.TrafficContext.TrafficIncidentType;
+ */
 
 public class TrafficIncidentDetails {
     TrafficIncidentType incidentType;
