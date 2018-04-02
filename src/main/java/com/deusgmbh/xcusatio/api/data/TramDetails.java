d821d91a92c322b11b67368ba27e0c81491d21ef
affeb <affeb@DESKTOP-AJ3K01H.fritz.box>
2018-04-02 23:31:04 +0200

    tram details filling complete

--- src/main/java/com/deusgmbh/xcusatio/api/data/TramDetails.java
+++ src/main/java/com/deusgmbh/xcusatio/api/data/TramDetails.java
@@ -10,8 +10,6 @@ import java.util.List;
 
 public class TramDetails {
     private String lineLabel;
-    private String firstEndStation;
-    private String secondEndstation;
     private String homeStation; /* the relevant station for the student */
     private int differenceTimeInMinutes; /*
                                           * in relation to normal time on
@@ -19,12 +17,9 @@ public class TramDetails {
                                           */
     private List<String> stops;
 
-    public TramDetails(String lineNumber, String firstEndStation, String secondEndstation, String homeStation,
-            List<String> stops) {
+    public TramDetails(String lineNumber, String homeStation, List<String> stops) {
         super();
         this.lineLabel = lineNumber;
-        this.firstEndStation = firstEndStation;
-        this.secondEndstation = secondEndstation;
         this.homeStation = homeStation;
         this.stops = stops;
     }
@@ -35,22 +30,6 @@ public class TramDetails {
 
     public void setLineLabel(String lineLabel) {
         this.lineLabel = lineLabel;
-    }
-
-    public String getFirstEndStation() {
-        return firstEndStation;
-    }
-
-    public void setFirstEndStation(String firstEndStation) {
-        this.firstEndStation = firstEndStation;
-    }
-
-    public String getSecondEndstation() {
-        return secondEndstation;
-    }
-
-    public void setSecondEndstation(String secondEndstation) {
-        this.secondEndstation = secondEndstation;
     }
 
     public List<String> getStops() {
