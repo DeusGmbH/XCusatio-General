0e7325560e3ba021b4b4f0a932e50a509653f308
affeb <affeb@DESKTOP-AJ3K01H.fritz.box>
2018-04-02 18:54:17 +0200

    changed fields

--- src/main/java/com/deusgmbh/xcusatio/api/data/TramDetails.java
+++ src/main/java/com/deusgmbh/xcusatio/api/data/TramDetails.java
@@ -12,13 +12,20 @@ public class TramDetails {
     private String lineLabel;
     private String firstEndStation;
     private String secondEndstation;
+    private String homeStation; /* the relevant station for the student */
+    private int differenceTimeInMinutes; /*
+                                          * in relation to normal time on
+                                          * homeStation
+                                          */
     private List<String> stops;
 
-    public TramDetails(String lineNumber, String firstEndStation, String secondEndstation, List<String> stops) {
+    public TramDetails(String lineNumber, String firstEndStation, String secondEndstation, String homeStation,
+            List<String> stops) {
         super();
         this.lineLabel = lineNumber;
         this.firstEndStation = firstEndStation;
         this.secondEndstation = secondEndstation;
+        this.homeStation = homeStation;
         this.stops = stops;
     }
 
@@ -52,6 +59,14 @@ public class TramDetails {
 
     public void setStops(List<String> stops) {
         this.stops = stops;
+    }
+
+    public String getHomeStation() {
+        return homeStation;
+    }
+
+    public void setHomeStation(String homeStation) {
+        this.homeStation = homeStation;
     }
 
 }
