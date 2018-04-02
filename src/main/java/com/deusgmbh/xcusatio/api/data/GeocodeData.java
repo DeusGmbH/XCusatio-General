3a37edd85252ef8fb2f2a369c96b21147ec8348e
affeb <affeb@DESKTOP-AJ3K01H.fritz.box>
2018-04-02 09:08:53 +0200

    extended GeocodeData fields (constructor, getters & setters accordingly)

--- src/main/java/com/deusgmbh/xcusatio/api/data/GeocodeData.java
+++ src/main/java/com/deusgmbh/xcusatio/api/data/GeocodeData.java
@@ -4,44 +4,91 @@ import com.deusgmbh.xcusatio.data.usersettings.Address;
 
 public class GeocodeData {
 
-    private Address address;
-    private double[] addressSpotCoordinates;
-    private double[][] boundingBoxCoordinates;
+	private Address address;
+	private double[] spotCoordinates;
+	private int[] mapTilesLowZoom;
+	private int[] mapTilesMediumZoom;
+	private int[] mapTilesHighZoom;
+	private double[][] boundingBoxCoordinates;
+	private String quadkey;
 
-    public GeocodeData(Address address, double[] addressSpotCoordinates, double[][] boundingBoxCoordinates) {
-        super();
-        this.address = address;
-        this.addressSpotCoordinates = addressSpotCoordinates;
-        this.boundingBoxCoordinates = boundingBoxCoordinates;
-    }
+	public GeocodeData(Address address, double[] spotCoordinates, int[] mapTilesLowZoom, int[] mapTilesMediumZoom,
+			int[] mapTilesHighZoom) {
+		super();
+		this.address = address;
+		this.spotCoordinates = spotCoordinates;
+		this.mapTilesLowZoom = mapTilesLowZoom;
+		this.mapTilesMediumZoom = mapTilesMediumZoom;
+		this.mapTilesHighZoom = mapTilesHighZoom;
+	}
 
-    public GeocodeData(Address address) {
-        super();
-        this.address = address;
-    }
+	public GeocodeData(Address address) {
+		super();
+		this.address = address;
+	}
 
-    public Address getAddress() {
-        return address;
-    }
+	public Address getAddress() {
+		return address;
+	}
 
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
-    public double[] getAddressSpotCoordinates() {
-        return addressSpotCoordinates;
-    }
+	public double[] getAddressSpotCoordinates() {
+		return spotCoordinates;
+	}
 
-    public void setAddressSpotCoordinates(double[] addressSpotCoordinates) {
-        this.addressSpotCoordinates = addressSpotCoordinates;
-    }
+	public void setAddressSpotCoordinates(double[] addressSpotCoordinates) {
+		this.spotCoordinates = addressSpotCoordinates;
+	}
 
-    public double[][] getBoundingBoxCoordinates() {
-        return boundingBoxCoordinates;
-    }
+	public double[][] getBoundingBoxCoordinates() {
+		return boundingBoxCoordinates;
+	}
 
-    public void setBoundingBoxCoordinates(double[][] boundingBoxCoordinates) {
-        this.boundingBoxCoordinates = boundingBoxCoordinates;
-    }
+	public void setBoundingBoxCoordinates(double[][] boundingBoxCoordinates) {
+		this.boundingBoxCoordinates = boundingBoxCoordinates;
+	}
+
+	public String getQuadkey() {
+		return quadkey;
+	}
+
+	public void setQuadkey(String quadkey) {
+		this.quadkey = quadkey;
+	}
+
+	public double[] getSpotCoordinates() {
+		return spotCoordinates;
+	}
+
+	public void setSpotCoordinates(double[] spotCoordinates) {
+		this.spotCoordinates = spotCoordinates;
+	}
+
+	public int[] getMapTilesLowZoom() {
+		return mapTilesLowZoom;
+	}
+
+	public void setMapTilesLowZoom(int[] mapTilesLowZoom) {
+		this.mapTilesLowZoom = mapTilesLowZoom;
+	}
+
+	public int[] getMapTilesMediumZoom() {
+		return mapTilesMediumZoom;
+	}
+
+	public void setMapTilesMediumZoom(int[] mapTilesMediumZoom) {
+		this.mapTilesMediumZoom = mapTilesMediumZoom;
+	}
+
+	public int[] getMapTilesHighZoom() {
+		return mapTilesHighZoom;
+	}
+
+	public void setMapTilesHighZoom(int[] mapTilesHighZoom) {
+		this.mapTilesHighZoom = mapTilesHighZoom;
+	}
 
 }
