39f2dd719d123ce4e420cb79a6bd73b212f9a1e8
affeb <affeb@DESKTOP-AJ3K01H.fritz.box>
2018-04-02 09:07:18 +0200

    added methods for processing api responses etc.

--- src/main/java/com/deusgmbh/xcusatio/api/services/TrafficAPI.java
+++ src/main/java/com/deusgmbh/xcusatio/api/services/TrafficAPI.java
@@ -1,8 +1,17 @@
 package com.deusgmbh.xcusatio.api.services;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Logger;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.deusgmbh.xcusatio.api.APIService;
 import com.deusgmbh.xcusatio.api.data.GeocodeData;
@@ -14,10 +23,43 @@ import com.deusgmbh.xcusatio.api.data.TrafficIncidentType;
 import com.deusgmbh.xcusatio.context.wildcard.TrafficContext;
 import com.deusgmbh.xcusatio.data.usersettings.Address;
 import com.deusgmbh.xcusatio.data.usersettings.UserSettings;
+import com.deusgmbh.xcusatio.data.usersettings.UserSettings.Sex;
 
 public class TrafficAPI extends APIService {
-
     private static final Logger LOGGER = Logger.getLogger(TrafficAPI.class.getName());
+
+    private final String BASE_URL = "https://traffic.cit.api.here.com/traffic/6.2/incidents/json/";
+    private final String APP_KEYS = "?app_id=ObXv79Ww3xdQ996uEDLw&app_code=74fsgcSubek54INvT13Rcg";
+    private final String JSONOB_TRAFFIC_ITEMS = "TRAFFIC_ITEMS";
+    private final String JSONARR_TRAFFIC_ITEM = "TRAFFIC_ITEM";
+    private final String JSONOB_LOCATION = "LOCATION";
+    private final String JSONOB_LOCATION_DEFINED = "DEFINED";
+    private final String JSONOB_DEFINED_ORIGIN = "ORIGIN";
+    private final String JSONOB_ORIGIN_ROADWAY = "ROADWAY";
+    private final String JSONARR_ROADWAY_DESCRIPTION = "DESCRIPTION";
+    private final String JSONSTR_DESCRIPTION_VALUE = "VALUE";
+    private final String JSONSTR_INCIDENT_TYPE = "TRAFFIC_ITEM_TYPE_DESC";
+    private final String JSONSTR_INCIDENT_STATUS = "TRAFFIC_ITEM_STATUS_SHORT_DESC";
+    private final String JSONARR_INCIDENT_DESCRIPTION = "TRAFFIC_ITEM_DESCRIPTION";
+    private final String JSONSTR_INCIDENT_DESCRIPTION_FIRST = "value";
+    private final String JSONSTR_INCIDENT_ENTRY_TIME = "ENTRY_TIME";
+    private final String JSONSTR_INCIDENT_END_TIME = "END_TIME";
+    private final String JSONOB_INCIDENT_CRITICALITY = "CRITICALITY";
+    private final String JSONSTR_CRITICALITY_DESCRIPTION = "DESCRIPTION";
+    private final String JSONOB_TRAFFIC_ITEM_DETAIL = "TRAFFIC_ITEM_DETAIL";
+    private final String JSONSTR_DETAIL_ROAD_CLOSED = "ROAD_CLOSED";
+
+    // https://traffic.cit.api.here.com/traffic/6.0/incidents.json
+
+    private String jsonResponse;
+    private GeocodeData geocodeData;
+    private String mapLowText;
+    private String mapMediumText;
+    private String mapHighText;
+    private URL requestUrlLow, requestUrlMedium, requestUrlHigh;
+    private List<TrafficIncidentDetails> incidentDetails;
+    private List<TrafficIncidentLocation> incidentLocation;
+    private List<TrafficIncidentTimes> incidentTimes;
 
     public TrafficAPI() {
 
@@ -30,10 +72,14 @@ public class TrafficAPI extends APIService {
          * 
          * 
          */
-        TrafficIncidentDetails trd = new TrafficIncidentDetails(TrafficIncidentType.CONSTRUCTION,
-                TrafficIncidentStatus.ACTIVE, "Strasse wegen Baustelle gesperrt");
-        TrafficIncidentLocation tri = new TrafficIncidentLocation(
-                new GeocodeData(new Address("Coblitzallee", "6", "68163", "Mannheim")));
+        List<TrafficIncidentDetails> trdList = new LinkedList<>();
+        trdList.add(new TrafficIncidentDetails(TrafficIncidentType.CONSTRUCTION, TrafficIncidentStatus.ACTIVE,
+                "Strasse wegen Baustelle gesperrt"));
+
+        List<TrafficIncidentLocation> triList = new LinkedList<>();
+        triList.add(
+                new TrafficIncidentLocation(new GeocodeData(new Address("Coblitzallee", "6", "68163", "Mannheim"))));
+
         String dateFormat = "MM/dd/yyyy hh:mm:ss";
         Date startTime = null, endTime = null;
         try {
@@ -42,22 +88,13 @@ public class TrafficAPI extends APIService {
         } catch (Exception e) {
             LOGGER.info("parsed wrong date format, " + e.getMessage());
         }
-        TrafficIncidentTimes trt = new TrafficIncidentTimes(startTime, endTime);
-        TrafficContext trafficContext = new TrafficContext(trd, tri, trt);
-        return trafficContext;
-        /*
-         * end of dummy context
-         * 
-         * 
-         */
 
-        // TODO getTrafficIncidentType
-        // TODO getTrafficIncidentShortDescription
-        // TODO getTrafficIncidentDescription
-        // TODO getTrafficIncidentLocationStreet
-        // TODO setTrafficIncidentLocationRest --> obtained from userSettings
-        // TODO getTrafficIncidentStartTime
-        // TODO getTrafficIncidentEndTime
+        List<TrafficIncidentTimes> trtList = new LinkedList<>();
+        trtList.add(new TrafficIncidentTimes(startTime, endTime));
+
+        TrafficContext trafficContext = new TrafficContext(trdList, triList, trtList);
+        return trafficContext;
+
     }
 
     @Override
@@ -66,17 +103,130 @@ public class TrafficAPI extends APIService {
     }
 
     @Override
-    public void extractDesiredInfoFromResponse() {
+    public void extractDesiredInfoFromResponse() throws JSONException {
+        getResponseFromWebsite();
+        JSONObject jsonTotal = new JSONObject(this.jsonResponse);
+        if (jsonTotal.has(JSONOB_TRAFFIC_ITEMS)) {
+            JSONObject trafficItems = jsonTotal.getJSONObject(JSONOB_TRAFFIC_ITEMS);
+            System.out.println("* " + trafficItems.toString());
+            if (trafficItems.has(JSONARR_TRAFFIC_ITEM)) {
+                JSONArray trafficItem = trafficItems.getJSONArray(JSONARR_TRAFFIC_ITEM);
+                List<JSONObject> trafficItemList = new LinkedList<>();
+                for (int i = 0; i < trafficItem.length(); ++i) {
+                    trafficItemList.add(trafficItem.getJSONObject(i));
+                    System.out.println("Traffic item " + trafficItem.get(i));
+                }
+
+                List<JSONObject> locationList = goInside(trafficItemList, JSONOB_LOCATION);
+
+                List<JSONObject> definedLocations = goInside(locationList, JSONOB_LOCATION_DEFINED);
+
+                List<JSONObject> locOrigins = goInside(definedLocations, JSONOB_DEFINED_ORIGIN);
+
+                List<JSONObject> roadways = new LinkedList<>();
+
+                List<String> roadwayDescriptionValues = new LinkedList<>();
+                for (JSONObject rdWay : locOrigins) {
+                    if (rdWay.has(JSONOB_ORIGIN_ROADWAY)) {
+                        roadways.add(rdWay.getJSONObject(JSONOB_ORIGIN_ROADWAY));
+                        System.out.println("Roadway: " + rdWay.toString());
+                        if (roadways.get(0)
+                                .has(JSONSTR_DESCRIPTION_VALUE)) {
+                            roadwayDescriptionValues.add(roadways.get(0)
+                                    .getString(JSONSTR_DESCRIPTION_VALUE));
+                        }
+                    }
+                }
+                for (String rwDescVal : roadwayDescriptionValues) {
+                    System.out.println("Roadway description values: " + rwDescVal);
+                }
+
+                List<JSONArray> roadwayDescriptions = new LinkedList<>();
+                for (JSONObject rdWay : roadways) {
+                    if (rdWay.has(JSONARR_ROADWAY_DESCRIPTION)) {
+                        roadwayDescriptions.add(rdWay.getJSONArray(JSONARR_ROADWAY_DESCRIPTION));
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    private List<JSONObject> goInside(List<JSONObject> into, String KEY) throws JSONException {
+        List<JSONObject> jL = new LinkedList<>();
+        for (JSONObject jO : into) {
+            if (jO.has(KEY)) {
+                jL.add(jO.getJSONObject(KEY));
+                System.out.println(KEY + ": " + jO.toString());
+            }
+        }
+        return jL;
+    }
+
+    @Override
+    public void printResponse() {
+        // TODO Auto-generated method stub
 
     }
 
-    // public static void main(String[] selenium)
-    // throws MalformedURLException, IOException, ParserConfigurationException,
-    // SAXException {
-    // TrafficAPI trafficAPI = new TrafficAPI();
-    // TrafficContext trafficContext = trafficAPI.get(null);
-    // trafficContext.logContextContent();
-    //
-    // }
+    @Override
+    public void getResponseFromWebsite() {
+        try {
+            getJsonStringFromInputStream();
+        } catch (IOException e) {
+            LOGGER.warning("Error getting jsonString from input stream: " + e.getMessage());
+        }
+        this.jsonResponse = getResponseAsJsonString();
+        System.out.println(this.jsonResponse);
+    }
 
+    private void prepareUrlBuild(GeocodeData gcd, UserSettings usersettings) {
+        int[] mapLow = gcd.getMapTilesLowZoom();
+        int[] mapMedium = gcd.getMapTilesMediumZoom();
+        int[] mapHigh = gcd.getMapTilesHighZoom();
+        this.mapLowText = "16/" + String.valueOf(mapLow[0]) + "/" + String.valueOf(mapLow[1]);
+        this.mapMediumText = "12/" + String.valueOf(mapMedium[0]) + "/" + String.valueOf(mapMedium[1]);
+        this.mapHighText = "8/" + String.valueOf(mapHigh[0]) + "/" + String.valueOf(mapHigh[1]);
+    }
+
+    @Override
+    public void buildRequestUrl(UserSettings usersettings) {
+        GeocodeAPI gApi = new GeocodeAPI();
+        GeocodeData gcd = gApi.get(usersettings);
+        prepareUrlBuild(gcd, usersettings);
+
+        System.out.println(BASE_URL + mapMediumText + APP_KEYS);
+
+        try {
+            super.setRequestUrl(new URL(BASE_URL + mapMediumText + APP_KEYS));
+        } catch (MalformedURLException e) {
+            // TODO: handle exception
+        }
+        System.out.println(getRequestUrl());
+
+    }
+
+    /*
+     * 
+     * test section
+     * 
+     * 
+     * 
+     */
+    public static void main(String[] uranium) {
+        TrafficAPI tApi = new TrafficAPI();
+        UserSettings usersettings = new UserSettings(null, null, Sex.MALE,
+                new Address("6", "Augartenstraﬂe", "68165", "Mannheim"));
+
+        tApi.buildRequestUrl(usersettings);
+        try {
+            tApi.extractDesiredInfoFromResponse();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // TrafficContext tContext = tApi.get(usersettings);
+        // tContext.logContextContent();
+    }
 }
