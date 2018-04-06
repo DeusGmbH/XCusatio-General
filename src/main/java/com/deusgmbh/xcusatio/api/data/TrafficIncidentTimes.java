2d49b51c689557517d024a6e48da9c701f9fcfb0
affeb <affeb@DESKTOP-AJ3K01H.fritz.box>
2018-04-06 23:22:47 +0200

    removed start time of incidents

--- src/main/java/com/deusgmbh/xcusatio/api/data/TrafficIncidentTimes.java
+++ src/main/java/com/deusgmbh/xcusatio/api/data/TrafficIncidentTimes.java
@@ -10,7 +10,6 @@ import java.util.Date;
  */
 
 public class TrafficIncidentTimes extends TimeFormattingUtils {
-    private Date startTimeOfTrafficIncident;
     private Date endTimeOfTrafficIncident;
 
     public TrafficIncidentTimes(Date startTimeOfTrafficIncident, Date endTimeOfTrafficIncident) {
@@ -18,16 +17,7 @@ public class TrafficIncidentTimes extends TimeFormattingUtils {
         // TODO keep in mind that String of API call has to be converted to Date
         // somewhere in the TrafficAPI class
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss");
-        this.startTimeOfTrafficIncident = startTimeOfTrafficIncident;
         this.endTimeOfTrafficIncident = endTimeOfTrafficIncident;
-    }
-
-    public Date getStartTimeOfTrafficIncident() {
-        return startTimeOfTrafficIncident;
-    }
-
-    public void setStartTimeOfTrafficIncident(Date startTimeOfTrafficIncident) {
-        this.startTimeOfTrafficIncident = startTimeOfTrafficIncident;
     }
 
     public Date getEndTimeOfTrafficIncident() {
