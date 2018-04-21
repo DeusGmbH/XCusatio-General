851c735965ef5b02f5eb7d22a47ed2e71c187f5b
tobiasxschmidt <tsbaunatal@gmail.com>
2018-04-21 17:20:46 +0200

    Reworked TrafficType Translations

--- src/main/java/com/deusgmbh/xcusatio/data/EnumTranslator.java
+++ src/main/java/com/deusgmbh/xcusatio/data/EnumTranslator.java
@@ -12,49 +12,50 @@ import com.deusgmbh.xcusatio.data.tags.Tag;
  *
  */
 public class EnumTranslator {
-	public static String toGerman(Tag tag) {
-		switch (tag) {
-		default:
-			return tag.toString();
-		}
-	}
+    public static String toGerman(Tag tag) {
+        switch (tag) {
+        default:
+            return tag.toString();
+        }
+    }
 
-	public static List<String> toGerman(List<Tag> tagList) {
-		List<String> translatedTagList = new ArrayList<String>();
-		tagList.stream().forEach(tag -> {
-			translatedTagList.add(toGerman(tag));
-		});
-		return translatedTagList;
-	}
+    public static List<String> toGerman(List<Tag> tagList) {
+        List<String> translatedTagList = new ArrayList<String>();
+        tagList.stream()
+                .forEach(tag -> {
+                    translatedTagList.add(toGerman(tag));
+                });
+        return translatedTagList;
+    }
 
-	public static String toGerman(TrafficIncidentType enumToTranslate) {
-		switch (enumToTranslate) {
-		case ACCIDENT:
-			return "Unfall";
-		case CONGESTION:
-			return "\u00DCberlastung";
-		case DISABLED_VEHICLE:
-			return "fahrunf\u00E4higes Fahrzeug";
-		case ROAD_HAZARD:
-			return "Gefahrenstelle";
-		case CONSTRUCTION:
-			return "Baustelle";
-		case PLANNED_EVENT:
-			return "geplante Sperrung";
-		case MASS_TRANSIT:
-			return "stockender Verkehr";
-		case OTHER_NEWS:
-			return "anderer Zwischenfall";
-		case WEATHER:
-			return "Wetter";
-		case MISC:
-			return "sonstiges";
-		case ROAD_CLOSURE:
-			return "Stra\u00DFensperrung";
-		case LANE_RESTRICTION:
-			return "Engstelle";
-		default:
-			return null;
-		}
-	}
+    public static String toGerman(TrafficIncidentType enumToTranslate) {
+        switch (enumToTranslate) {
+        case ACCIDENT:
+            return "ein Unfall";
+        case CONGESTION:
+            return "eine \u00DCberlastung";
+        case DISABLED_VEHICLE:
+            return "ein fahrunf\u00E4higes Fahrzeug";
+        case ROAD_HAZARD:
+            return "eine Gefahrenstelle";
+        case CONSTRUCTION:
+            return "eine Baustelle";
+        case PLANNED_EVENT:
+            return "eine geplante Sperrung";
+        case MASS_TRANSIT:
+            return "stockender Verkehr";
+        case OTHER_NEWS:
+            return "eine Baustelle";
+        case WEATHER:
+            return "schlechte Wetterbedingungen";
+        case MISC:
+            return "kein durchkommen";
+        case ROAD_CLOSURE:
+            return "eine Stra\u00DFensperrung";
+        case LANE_RESTRICTION:
+            return "eine gesperrte Spur";
+        default:
+            return null;
+        }
+    }
 }
