07caa78eb9830faecf3632a0df58049471ab29ce
affeb <affeb@DESKTOP-AJ3K01H>
2018-04-22 12:00:01 +0200

    fixed typo; translated tags and ScenarioType to German

--- src/main/java/com/deusgmbh/xcusatio/data/scenarios/ScenarioType.java
+++ src/main/java/com/deusgmbh/xcusatio/data/scenarios/ScenarioType.java
@@ -2,4 +2,21 @@ package com.deusgmbh.xcusatio.data.scenarios;
 
 public enum ScenarioType {
     WHEELOFFORTUNE, DELAYED_SUBMISSION, LATE_ARRIVAL, THUMBGESTURE;
+
+    @Override
+    public String toString() {
+        switch (this) {
+        case WHEELOFFORTUNE:
+            return "Gl\u00fccksrad";
+        case DELAYED_SUBMISSION:
+            return "Versp\u00e4tete Abgabe";
+        case LATE_ARRIVAL:
+            return "Zusp\u00e4tkommen";
+        case THUMBGESTURE:
+            return "Daumengeste";
+
+        default:
+            return super.toString();
+        }
+    }
 }
